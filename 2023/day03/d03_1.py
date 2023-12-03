8a86e7401acb7f957e8e93e2c7b0e476a641d5cc
davmacario <dav.macario@gmail.com>
2023-12-03 01:55:30 -0600

    Update d03 code - readability

--- 2023/day03/d03_1.py
+++ 2023/day03/d03_1.py
@@ -2,6 +2,27 @@ import numpy as np
 
 tot = 0
 
+
+def get_around(y: int, x1: int, x2: int):
+    around = [
+        (max(0, y - 1), max(0, x1 - 1)),  # TL
+        (y, max(0, x1 - 1)),  # CL
+        (min(n_lines - 1, y + 1), max(0, x1 - 1)),  # BL
+        (max(0, y - 1), min(lines.shape[1] - 1, x2)),  # TR
+        (y, min(lines.shape[1] - 1, x2)),  # CR
+        (
+            min(n_lines - 1, y + 1),
+            min(lines.shape[1] - 1, x2),
+        ),  # BR
+    ]
+    # Add the ones above and below digits
+    for index in range(x1, x2):
+        around.append((max(0, y - 1), index))
+        around.append((min(n_lines - 1, y + 1), index))
+
+    return around
+
+
 with open("./in.txt") as f:
     lines = np.array([list(line.rstrip()) for line in f])
     print(lines)
@@ -24,30 +45,15 @@ with open("./in.txt") as f:
 
                 # NOTE: the columns of the current number go from j to ind
                 # (included)
-
                 n_lst = [c_curr] + others
 
                 # Full "isolated" number:
                 n_curr = int("".join(n_lst))
 
                 # Indices of positions around current one:
-                around = [
-                    (max(0, i - 1), max(0, j - 1)),  # TL
-                    (i, max(0, j - 1)),  # CL
-                    (min(n_lines - 1, i + 1), max(0, j - 1)),  # BL
-                    (max(0, i - 1), min(lines.shape[1] - 1, ind)),  # TR
-                    (i, min(lines.shape[1] - 1, ind)),  # CR
-                    (
-                        min(n_lines - 1, i + 1),
-                        min(lines.shape[1] - 1, ind),
-                    ),  # BR
-                ]
-                # Add the ones above and below digits
-                for index in range(j, ind):
-                    around.append((max(0, i - 1), index))
-                    around.append((min(n_lines - 1, i + 1), index))
-
+                around = get_around(i, j, ind)
                 to_be_added = False
+
                 for k in around:
                     x = lines[k]
                     if not x.isalpha() and not x.isdigit() and x != ".":
