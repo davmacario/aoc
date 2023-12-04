cd6c448bd7d2cbaf40d7cca2465e55433e820d34
davmacario <dav.macario@gmail.com>
2023-12-03 21:14:25 -0600

    Update d01 code

--- 2023/day01/day01_1.py
+++ 2023/day01/day01_1.py
@@ -1,23 +1,24 @@
-tot = 0
-with open("./in.txt", "r") as f:
-    for l in f:
-        firstdigit = -1
-        lastdigit = -1
+if __name__ == "__main__":
+    tot = 0
+    with open("./in.txt", "r") as f:
+        for l in f:
+            firstdigit = -1
+            lastdigit = -1
 
-        l_as_lst = list(l)
-        ind = 0
+            l_as_lst = list(l)
+            ind = 0
 
-        while firstdigit == -1:
-            if l_as_lst[ind].isdigit():
-                firstdigit = l_as_lst[ind]
-            ind += 1
+            while firstdigit == -1:
+                if l_as_lst[ind].isdigit():
+                    firstdigit = l_as_lst[ind]
+                ind += 1
 
-        ind = len(l_as_lst) - 1
-        while lastdigit == -1:
-            if l_as_lst[ind].isdigit():
-                lastdigit = l_as_lst[ind]
-            ind -= 1
+            ind = len(l_as_lst) - 1
+            while lastdigit == -1:
+                if l_as_lst[ind].isdigit():
+                    lastdigit = l_as_lst[ind]
+                ind -= 1
 
-        tot += int("".join([str(firstdigit), str(lastdigit)]))
+            tot += int("".join([str(firstdigit), str(lastdigit)]))
 
-print(tot)
+    print(tot)
