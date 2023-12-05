705f03f453cfc2cfc0e7b8388e636e9ee4f213f7
davmacario <dav.macario@gmail.com>
2023-12-05 00:25:14 -0600

    Solve with threading - proud of myself

--- 2023/day05/d05.py
+++ 2023/day05/d05.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
 
+import threading
+
 import numpy as np
 
 DEBUG = True
@@ -36,6 +38,59 @@ def do_mapping(in_val, li, lo, ranges):
         i += 1
 
     return out_val
+
+
+def heart_function(lines, curr_values, sol_lst, sol_ind):
+    """
+    Center of the loop - make it thread for each range of seeds
+
+    Args:
+        lines: file lines
+        curr_values: input values for this instance
+        sol_list: ptr to list with minima (solution)
+        sol_ind: index of current run into sol_lst
+    """
+
+    last_key = "seed"
+
+    values = curr_values
+
+    i = 2  # Line index
+    while i < len(lines):
+        # Check where we're at - seeds, map (or values)
+        if DEBUG:
+            print(f"Line {i}")
+        ln = lines[i]
+
+        if ln.endswith("map:") and ln.split(" ")[0].split("-")[0] == last_key:
+            assert values is not None
+
+            if DEBUG:
+                print(f"New map: {ln.split(' ')[0]}")
+            # Map beginning!
+            list_out = []  # Will contain the input mapping starting values
+            list_in = []  # Will contain the output mapping starting values
+            ranges = []  # Will contain the ranges
+            # -> get the values
+            i += 1
+            while i < len(lines) and lines[i] != "":
+                list_out.append(int(lines[i].split(" ")[0]))
+                list_in.append(int(lines[i].split(" ")[1]))
+                ranges.append(int(lines[i].split(" ")[2]))
+                i += 1
+
+            if DEBUG:
+                print("> Map obtained!")
+
+            # Values stored - invoke function
+            values = do_mapping(values, list_in, list_out, ranges)
+            last_key = ln.split(" ")[0].split("-")[2]
+        else:
+            raise RuntimeError(f"Shouldn't be here - Line [{i}]:\n{ln}")
+
+        i += 1
+
+    sol_lst[sol_ind] = min(values)
 
 
 if __name__ == "__main__":
@@ -124,61 +179,23 @@ if __name__ == "__main__":
 
             assert len(seed_start) == len(seed_range)
 
-            mins = []
+            mins = [0] * len(seed_start)
+
+            thr_lst = []
 
             for k in range(len(seed_start)):
-                last_key = "seed"
-
-                values = list(
+                vals_k = list(
                     range(seed_start[k], seed_start[k] + seed_range[k])
                 )
 
-                i = 2  # Line index
-                while i < len(lines):
-                    # Check where we're at - seeds, map (or values)
-                    if DEBUG:
-                        print(f"Line {i}")
-                    ln = lines[i]
+                t = threading.Thread(
+                    target=heart_function, args=[lines, vals_k, mins, k]
+                )
+                t.start()
+                thr_lst.append(t)
 
-                    if (
-                        ln.endswith("map:")
-                        and ln.split(" ")[0].split("-")[0] == last_key
-                    ):
-                        assert values is not None
-
-                        if DEBUG:
-                            print(f"New map: {ln.split(' ')[0]}")
-                        # Map beginning!
-                        list_out = (
-                            []
-                        )  # Will contain the input mapping starting values
-                        list_in = (
-                            []
-                        )  # Will contain the output mapping starting values
-                        ranges = []  # Will contain the ranges
-                        # -> get the values
-                        i += 1
-                        while i < len(lines) and lines[i] != "":
-                            list_out.append(int(lines[i].split(" ")[0]))
-                            list_in.append(int(lines[i].split(" ")[1]))
-                            ranges.append(int(lines[i].split(" ")[2]))
-                            i += 1
-
-                        if DEBUG:
-                            print("> Map obtained!")
-
-                        # Values stored - invoke function
-                        values = do_mapping(values, list_in, list_out, ranges)
-                        last_key = ln.split(" ")[0].split("-")[2]
-                    else:
-                        raise RuntimeError(
-                            f"Shouldn't be here - Line [{i}]:\n{ln}"
-                        )
-
-                    i += 1
-
-                f.close()
-
-            mins.append(min(values))
+            # Wait for threads to finish
+            for th in thr_lst:
+                th.join()
 
         print(f"Q2 sol: {min(mins)}")
