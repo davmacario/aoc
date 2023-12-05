a9ab2f8c9540b80048c784cebca0bd054806f575
davmacario <dav.macario@gmail.com>
2023-12-05 02:06:33 -0600

    Optimize

--- 2023/day05/d05.py
+++ 2023/day05/d05.py
@@ -27,7 +27,7 @@ def do_mapping(in_val, li, lo, ranges):
         n = in_val[i]
         found_range = False
         for j in range(len(li)):
-            if n in range(li[j], li[j] + ranges[j]) and not found_range:
+            if n >= li[j] and n < li[j] + ranges[j] and not found_range:
                 # Found input range
                 found_range = True
                 diff_in_start = n - li[j]
@@ -38,6 +38,61 @@ def do_mapping(in_val, li, lo, ranges):
         i += 1
 
     return out_val
+
+
+def create_maps(li, lo, ranges):
+    out = []
+
+    for i in range(len(li)):
+        out.append((lo[i], li[i], ranges[i]))
+
+    if DEBUG:
+        print(out)
+
+    return out
+
+
+def map_seed(seed, maps):
+    """Get location from seed"""
+    keys = list(maps.keys())
+
+    val = seed
+    last_type = "seed"
+
+    for k in keys:
+        assert last_type == k.split("-")[0]
+        found = False
+        for j in range(len(maps[k])):
+            if (
+                not found
+                and val >= maps[k][j][1]
+                and val < maps[k][j][1] + maps[k][j][2]
+            ):
+                found = True
+                val = maps[k][j][0] + (val - maps[k][j][1])
+        last_type = k.split("-")[2]
+
+    return val
+
+
+def map_location(location, maps):
+    """Get seed from location"""
+    keys = list(maps.keys())[::-1]
+
+    val = location
+
+    for k in keys:
+        found = False
+        for j in range(len(maps[k])):
+            if (
+                not found
+                and val >= maps[k][j][0]
+                and val < maps[k][j][0] + maps[k][j][2]
+            ):
+                found = True
+                val = maps[k][j][1] + (val - maps[k][j][0])
+
+    return val
 
 
 def heart_function(lines, curr_values, sol_lst, sol_ind):
@@ -176,34 +231,83 @@ if __name__ == "__main__":
 
             assert len(seed_start) == len(seed_range)
 
-            mins = [0] * len(seed_start)
+            # Create the maps:
+            if DEBUG:
+                print("creating maps")
+            i = 2
+            last_key = "seed"
 
-            n_parall_thr = 2
+            maps = {}
 
-            for ind in range(int(np.ceil(len(seed_start) / n_parall_thr))):
-                thr_lst = []
+            while i < len(lines):
+                # Check where we're at - seeds, map (or values)
                 if DEBUG:
-                    print(
-                        f"Threads {n_parall_thr * ind} to \
-{min(len(seed_start), n_parall_thr * (ind + 1))}"
-                    )
+                    print(f"Line {i}")
+                ln = lines[i]
 
-                for k in range(
-                    n_parall_thr * ind,
-                    min(len(seed_start), n_parall_thr * (ind + 1)),
+                if (
+                    ln.endswith("map:")
+                    and ln.split(" ")[0].split("-")[0] == last_key
                 ):
-                    vals_k = list(
-                        range(seed_start[k], seed_start[k] + seed_range[k])
+                    assert values is not None
+
+                    if DEBUG:
+                        print(f"New map: {ln.split(' ')[0]}")
+                    # Map beginning!
+                    list_out = (
+                        []
+                    )  # Will contain the input mapping starting values
+                    list_in = (
+                        []
+                    )  # Will contain the output mapping starting values
+                    ranges = []  # Will contain the ranges
+                    # -> get the values
+                    i += 1
+                    while i < len(lines) and lines[i] != "":
+                        list_out.append(int(lines[i].split(" ")[0]))
+                        list_in.append(int(lines[i].split(" ")[1]))
+                        ranges.append(int(lines[i].split(" ")[2]))
+                        i += 1
+
+                    # Store map by name
+                    maps[ln.split(" ")[0]] = create_maps(
+                        list_in, list_out, ranges
                     )
 
-                    t = threading.Thread(
-                        target=heart_function, args=[lines, vals_k, mins, k]
-                    )
-                    t.start()
-                    thr_lst.append(t)
+                    last_key = ln.split(" ")[0].split("-")[2]
+                else:
+                    raise RuntimeError(f"Shouldn't be here - Line [{i}]:\n{ln}")
+                i += 1
 
-                # Wait for threads to finish
-                for th in thr_lst:
-                    th.join()
+            if DEBUG:
+                print("Maps created")
 
-        print(f"Q2 sol: {min(mins)}")
+            # From in to out:
+            if False:
+                min_out = -1
+                for k in range(len(seed_start)):
+                    for h in range(
+                        seed_start[k], seed_start[k] + seed_range[k]
+                    ):
+                        curr_out = map_seed(h, maps)
+                        if min_out == -1 or curr_out < min_out:
+                            if DEBUG:
+                                print(curr_out)
+                            min_out = curr_out
+
+                print(f"Q2 sol: {min_out}")
+                exit()
+
+            # From out to in
+            loc_ind = 0
+            while loc_ind >= 0:
+                print(loc_ind)
+                s = map_location(loc_ind, maps)
+
+                for k in range(len(seed_start)):
+                    if s >= seed_start[k] and s < seed_start[k] + seed_range[k]:
+                        print(f"Q2 solution: {loc_ind}")
+                        exit(1)
+                loc_ind += 1
+
+            # -----------------------------------------------------------
