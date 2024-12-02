6808ceba8a17ece62e6e6e6e745cd91e9271b992
Davide Macario <dav.macario@gmail.com>
2024-12-02 23:11:08 +0100

    feat: improve boilerplate
    Add file parsing (line by line)

--- 2024/boilerplate/main.go
+++ 2024/boilerplate/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
-    "fmt"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
+	input_file := "./in.txt"
+	f, err := os.Open(input_file)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	defer f.Close()
+
+	// Scanner to read the file
+	scanner := bufio.NewScanner(f)
+
+	// Iterate over lines of file
+	for scanner.Scan() {
+		line := scanner.Text()
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
